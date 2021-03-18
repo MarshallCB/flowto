4e52d562a44748e32620147e0928304940d992c1
Marshall Brandt <MarshallCB@users.noreply.github.com>
2021-03-18 13:43:04 -0500

    update readme

--- readme.md
+++ readme.md
@@ -13,3 +13,12 @@
     <img src="https://badgen.net/packagephobia/install/flowto" alt="install size" />
   </a>
 </div>
+
+# Overview
+- Conforms to `Iterable` and `Iterator` protocols (just like generators)
+- Supports `back()` and `forward()` (similar to browser navigation)
+- Serializable state (respawn at same position)
+  - `save((state) => { })` callback is invoked every time the state changes
+  - `load(state)` will return the iterator with equivalent state
+- Composable (sub-iterator states are maintained)
+
--- src/index.js
+++ src/index.js
@@ -1,103 +1,83 @@
-import { Model, Context } from 'ctex';
 
-let Thread = (flow) => Model({
-  // think of [null] as arguments. This would be fn() //null parameter
-  // may not need, if context state is enough instead of parameters
-  history: [[null]], // double array to keep track of nested histories
-  value: null,
-  done: false,
-  current: null,
-  set current({ value, done}={}){
-    if(done){
-      this.done = true;
-    }
-    this.value = value;
-    return { value, done};
-  },
-  inSubflow: false,
-  flow,
-  i: 0,
-  init(){
-    // Spring based on history
-    this[Symbol.iterator] = () => this
-  },
-  next(x){
-    do{
-      this.current = this.flow.next(this.save(x))
-    }while(this.i < this.history.length && !this.done) // sprint to catch up if behind
-    return this.current;
-  },
-  to(generator){
-    if(generator.subscribe){
-      generator.subscribe('history', (history) => {
-        this.resave(history)
-      })
-      generator.subscribe('done', (isDone, gen) => {
-        if(isDone){
-          this.resave(gen.value)
-        }
-      })
-    }
-    if(this.i < this.history.length-1){
-      // Mimic going into the original subthread and obtaining return value (all we care about)
-      return function*(){ return this.save() }.bind(this)()
-    } else if(this.i === this.history.length-1 && this.inSubflow){
-      if(this.inSubflow){
-        // TODO: return the "else" function, but with history passed into function
-      } else {
-        return function*(){ return this.save() }.bind(this)()
+
+export function flow(generator){
+
+  return function(){
+    let current;
+    let currentIndex = 0;
+    let local_memory = {};
+    let live_this = new Proxy({}, {
+      get(_,k){
+        return local_memory[k];
+      },
+      set(_,k,v){
+        local_memory[k] = v;
+        this.addHistory(v);
       }
+    })
+    let sprint_this = new Proxy({},{
+      get(_,k){
+        return local_memory[k];
+      },
+      set(_,k,v){
+        local_memory[k] = history[i++];
+      }
+    })
+    let stack = [{
+      history: [],
+      iterator: null
+    }]
+    let history_stack = [[]];
+    
+    let stack = [generator.apply(this_proxy, arguments)];
+    function save(val){
+      last(history_stack).push()
     }
-    else {
-      // TODO: REFACTOR to just literally return flow but subscribe to flow's `done` and `history` endpoints
-      return function*(withObj){
-        // TODO: mark as inside this inner flow
-        // TODO: subcribe to save() to keep up-to-date history
-        // maybe even a root subscribe to inner flow to update a this property (will get saved bc it's in values())
-        let subthread = generator(withObj);
-        subthread.subscribe(history, function(h){
-          this.resave(history); // replace the final value from history
-        }.bind(this))
-        this.inSubflow = false;
-        let a = yield* subthread;
-        this.inSubflow = false;
-        this.resave(a); // replace history with result from the generator
-      }.bind(this);
-    }
-  },
-  save(v){
-    if(this.i < this.history.length){
-      // when sprinting to current, return value from memory, not value passed
-      return this.history[this.i++]
-    } else { 
-      // when we don't have history, return the passed value, and remember it
-      this.i++;
-      this.history = [...this.history, v];
-      return v;
-    }
-  },
-  resave(v){
-    this.history[i]=v;
-    return v;
-  }
-})
+    return {
+      next(ans){
+        current = last(iterator_stack).next(ans);
+        if(current.done){
+          // We're done with this iterator
+          iterator_stack.pop();
+          // If there are still iterators left, send result to parent
+          // Else, we're done, send result { value, done: true }
+          if(iterator_stack.length > 0){
+            return this.next(current.value);
+          } else {
+            return current;
+          }
+        }
+        if(current.value && current.value[Symbol.iterator]){
 
+          // We've just received a new iterator
+          // eg: `yield otherGenerator()`
+          iterator_stack.push(current.value[Symbol.iterator](/* TODO: specialized this */));
+          // TODO: add to history stack
+          // TODO: channel saves, titles, progress into parent
+          // start new iterator
+          
+          return this.next(null);
+        } else {
+          return current;
+        }
+      },
+      back(distance){
+        // go back (by yields, not by saves)
+      },
+      load(history){
 
-// input: generator function
-// output: function that outputs an iterator (generator function)
-// function.with() => attach arguments to generator context
-// function[Symbol.iterator] = 
-let Flow = (flow) => {
-  let fn = function(){
-    let params = {...this,...arguments[0]}
-    let thread = Thread(flow(params))(params)
-    return thread
+      },
+      save(callback){
+
+      },
+      [Symbol.iterator]:  function(history) { return this }
+    }
   }
-  fn.with = function(obj){
-    return fn.bind({ ...fn, ...obj });
-  }
-  // fn[Symbol.iterator] = function(){ return this }
-  return fn;
 }
 
-// Idea: Set this[Symbol.iterator] on returned function from Flow that wraps inner generator
+
+let thread = {
+  history: [],
+  subthread: {},
+  
+}
